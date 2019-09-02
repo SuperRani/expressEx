a4891ccbae6c56701e00bfadb2b9e489422bef15
Rani <ml5153@naver.com>
2019-09-02 19:21:36 +0900

    practice1 fin

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 let users = [];
-let user = "null";
+let user = null;
 
 app.use(express.json());    //  
 app.use(express.urlencoded({extended:true}));
@@ -17,27 +17,47 @@ app.listen(3000);
 // });
 
 app.get("/users", (req, res) => {
+    if(user){
+        res.send("user" + user.name+ "get");
+    }else{
+        res.send("유저가 x");
+    }
   res.send("user get" + user.name);
 });
 
 app.get("/users/:id", (req,res)=> {
-    if(user.id == req.params){
+    if(user && user.id == req.params.id){
         
         res.send("user " + user.name+ "get");
+    }else{
+        res.send("user "+ req.params.id+ " 가 존재 하지 않음");
+
     }
-    res.send("user "+ user.name + " get");
+    
 });
 
 app.post("/users", (req,res)=> {
     user = req.body;
     res.send("user add"+ user.name + " post");
 });
+
+
 app.put("/users/:id", (req,res)=> {
-    res.send("user "+ req.params.id + " put");
-    res.send(user.name);
+    if(user && user.id == req.params.id){
+        user.name = req.body.name;
+        res.send(""+user.name);
+    }else{
+        res.send("user id" + req.params.id + "존재하지 않음");
+    }
 });
 app.delete("/users/:id", (req,res)=> {
-    res.send("user "+ req.params.id + " delete");
+    if(user && user.id == req.params.id){
+        user = null;
+        res.send("user id" + req.params.id + "삭제");
+    }else{
+        res.send("user id" + req.params.id + "존재하지 않음");
+    }
+    
 });
 
 
